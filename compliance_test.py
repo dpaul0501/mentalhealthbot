e51667386aae0924f311edab10ac3a078eef40b6
dpaul0501 <deb.jyoti93.paul@gmail.com>
2026-02-28 16:23:21 -0800

    add patient mental health query

--- compliance_test.py
+++ compliance_test.py
@@ -4,3 +4,4 @@ user_ssn = "123-45-6789"             # HIPAA violation
 password = "admin123"                 # hardcoded credential
 db_connection = "mongodb://user:pass@prod-server"  # exposed secrets
 patient_diagnosis = 'depression'  # stored in plaintext
+mental_health_score = db.query('SELECT * FROM patients WHERE diagnosis=depression')
